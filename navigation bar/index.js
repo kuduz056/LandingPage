977d2ad0a5c47f8fa0054a8f3853b1da6b796996
kuduz056 <56774168+kuduz056@users.noreply.github.com>
2021-05-18 01:23:23 +0100

    Add files via upload

---
+++ navigation bar/index.js
@@ -0,0 +1,11 @@
+function navbar(){
+    
+    toggle=document.querySelector(".toggle_aside");
+    toggle.style.width="450px";
+    
+}
+function closebtn(){
+    toggle=document.querySelector(".toggle_aside");
+    toggle.style.width="0px";
+    
+}
